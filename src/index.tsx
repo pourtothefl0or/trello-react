010002caae427eb8949c409126b8edb8ffdc9dfe
pourtothefl0or <pourtothefl0or@gmail.com>
2022-04-19 10:53:43 +0600

    Исправление export'ов

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/normalize.css';
 import './styles/common.css';
-import Index from './pages/Index';
+import { Index } from './pages/Index';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(<Index />);
